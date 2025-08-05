0e78645f2c27ea5bc6f4a57961329b2852f7f456
Rafal Oles <152035331+olraf24@users.noreply.github.com>
2025-08-05 18:54:20 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ZenInfoPortal from './components/ZenPortal.jsx'
+import './styles/globals.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ZenInfoPortal />
+  </React.StrictMode>,
+)
